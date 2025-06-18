01609f19df91b7e8aa6233d46ef6b725aa52e683
sumit <139071474+Sumitkumar181@users.noreply.github.com>
2025-06-18 08:17:45 +0530

    Local changes before pulling from remote

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { TbSlashes } from "react-icons/tb"
 
 export default function Services() {
   return (
-      <div>
-           
+    <div className=' bg-[#dadee0] w-full px-20 border py-10 lg:py-20 '>
+
+      
+      <div className='flex flex-col  gap-4 flex-1 border'>
+        <div className='border w-1/2'>
+          <div>
+        <div className='flex items-center gap-2'>
+          <TbSlashes className='text-lightBlue' size={24} />
+          <p className='text-darkBlue text-base sm:text-lg md:text-xl font-roboto font-medium'>
+            Our Services
+          </p>
+        </div>
+        <div>
+          <h1 className='text-darkBlue text-2xl sm:text-3xl md:text-5xl font-semibold font-roboto leading-tight'>
+            Services we provide to
+          </h1>
+          <h1 className='text-lightBlue text-2xl sm:text-3xl md:text-5xl font-semibold font-roboto leading-tight'>
+            Evaluate your business
+          </h1>
+            </div>
+          </div>
+          <div className='flex items-center justify-end flex-1'>
+            <button className='bg-lightBlue h-12 w-44 font-roboto rounded-full text-sm text-white'>
+              View All Services
+            </button>
+          </div>
+        </div>  
+        
+        
+      </div>
+
+      
+      
+
+      <div className=''>
+        <div className='w-80 h-44 bg-white'>
+
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AboutUs from '../components/AboutUs'
+import Services from '../components/Services'
 
 export default function HomePage() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <AboutUs/>
+      <AboutUs />
+      <Services/>
     </div>
   )
 }
